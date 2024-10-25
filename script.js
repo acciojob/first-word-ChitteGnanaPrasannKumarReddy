ce2bc1c3ee8bdd0b33129c0151a647a4a19b45b0
ChitteGnanaPrasannKumarReddy <116110463+ChitteGnanaPrasannKumarReddy@users.noreply.github.com>
2024-10-25 19:36:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-function firstWord(s) {
+function firstWord(s) { 
+	
   // your code here
+	let trimmedString = s.trim();
+    let spaceIndex = trimmedString.indexOf(" "); 
+
+	if (spaceIndex === -1) return trimmedString; 
+
+	return trimmedString.substring(0, spaceIndex);
 }
 
 // Do not change the code below
-
 const s = prompt("Enter String:");
 alert(firstWord(s));
